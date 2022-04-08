a7fbbd7cb7a12d8b8b1ff3710b09a63339c38783
Eric Taylor <eric.taylor1012@gmail.com>
2022-04-07 23:07:14 -0700

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work-Planner
+
+## Description
+This project helps us figure out how to implement Modal, work with bootstrap formatting, and continue to improve our save to localStorage.
+
+I learned that I still need to keep practicing the fundamentals of Javasript to continue to improve on my functionality there. 
+
+## Installation
+To properly install this program, I would 
+
+## Usage
+To use this application, you need to input whatever action item you want into the input field assosciated with the hour block. The 
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+$(document).ready(function(saveText) {
+    //listen for button clicks
+    $('.saveBtn').on('click', function () {
+       //get nearby values/time
+       var toDo = [{
+                work: $(this).siblings("input").val(),
+                hour: $(this).siblings("div").text(),
+            }]
+
+                saveArr.push(toDo)
+                console.log(toDo)
+                var schedule = JSON.stringify(saveArr)
+                
+        //save in local Storage
+        function saveText() {
+            textDiv.textContent = savedText
+            localStorage.setItem("textSaved", savedText)
+        }
+    });
+
+    function getText() {
+        var storedText = localStorage.getItem("textSaved")
+    }
+
 var currentDayEl = $('#currentDay')
 var hourEl = $(".hour")
 var textEl = $(".textDiv")
@@ -8,21 +32,11 @@ var saveArr = []
 // var hourText = Number(hourEl.text())
 // var timeText = Number(moment().format("HH"))
 
-
-//Moment.js global variables
-
-
-
 //use moment.js to display current date
 setInterval(function() {
-    var time = moment().format("h:mm:ss a");
+    var time = moment().format("MMMM Qo, YYYY");
 $("#currentDay").text(time); 
 },1000);
-
-// User fills in text using input box
-// User clicks the save button and the information gets put into local Storage
-
-// save text to console
 
 // function saveText() {
 //     var toDo = [{
@@ -40,6 +54,7 @@ $("#currentDay").text(time);
 //         JSON.stringify(schedule)
 //         localStorage.setItem("itinerary", schedule)
 //     } else {
+//         why
 //         saveArr.push(JSON.parse(localStorage.getItem("itinerary")))
 //         localStorage.setItem("itinerary", schedule)
 //     }
@@ -57,22 +72,8 @@ $("#currentDay").text(time);
 
 //save text to console
 
-$(document).ready(function(saveText) {
-    //listen for button clicks
-    saveBtnEl.onclick(saveText)
-    //get nearby values/time
-    var toDo = [{
-        work: $(this).siblings("input").val(),
-       hour: $(this).siblings("div").text(),
-           }]
-    //save in local Storage
-    localStorage.setItem("toDo", JSON.stringify(textEl))
 
-   
 
-});
-
-console.log(textEl)
 
 //Moment = time (.past)
 // if (hourText <  time) {
@@ -96,12 +97,12 @@ console.log(textEl)
 var time = moment();
 
 function trackTime() {
-    var timeSlot = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var timeSlot = [0900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700];
 
     for (var i = 0; i < timeSlot.length; i++) {
         var hour = timeSlot [i];
 
-    if (time > hourEl) {
+    if (time < hourEl) {
         document.querySelectorAll('.hour').forEach(function (dscrpB) {
             dscrpB.classList.add('past');
             console.log('Before worked');
@@ -121,3 +122,5 @@ function trackTime() {
 }
     }};
 trackTime();
+
+});
