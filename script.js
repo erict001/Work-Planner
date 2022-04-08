f75edada4a44d3c1dd5c9bd3d77905ab6938c3ad
Eric Taylor <eric.taylor1012@gmail.com>
2022-04-07 19:13:17 -0700

    moment almost working

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var currentDayEl = $('#currentDay')
-var hourEl = $(".hourPlace")
+var hourEl = $(".hour")
 var textEl = $(".textDiv")
 var saveBtnEl = $(".btn")
 var localPull = JSON.parse(localStorage.getItem("itinerary"))
 var saveArr = []
-var finish = localPull[0]
-var hourText = Number(hourEl.text())
-var timeText = Number(moment().format("HH"))
+// var finish = localPull[]
+// var hourText = Number(hourEl.text())
+// var timeText = Number(moment().format("HH"))
+
 
 //Moment.js global variables
-var time = moment()
-var beforeTime = 
+
 
 
 //use moment.js to display current date
@@ -24,58 +24,100 @@ $("#currentDay").text(time);
 
 // save text to console
 
-function saveText() {
+// function saveText() {
+//     var toDo = [{
+//         work: $(this).siblings("input").val(),
+//         hour: $(this).siblings("div").text(),
+//     }]
+
+//     saveArr.push(toDo)
+
+//     console.log(toDo)
+
+//     var schedule = JSON.stringify(saveArr)
+
+//     if (localPull === null) {
+//         JSON.stringify(schedule)
+//         localStorage.setItem("itinerary", schedule)
+//     } else {
+//         saveArr.push(JSON.parse(localStorage.getItem("itinerary")))
+//         localStorage.setItem("itinerary", schedule)
+//     }
+// };
+// saveBtnEl.click(saveText)
+// console.log(saveBtnEl)
+
+
+// function renderMessage() {
+
+//     if (localPull !== null) {
+        
+//     }
+//   }
+
+//save text to console
+
+$(document).ready(function(saveText) {
+    //listen for button clicks
+    saveBtnEl.onclick(saveText)
+    //get nearby values/time
     var toDo = [{
         work: $(this).siblings("input").val(),
-        hour: $(this).siblings("div").text(),
-    }]
+       hour: $(this).siblings("div").text(),
+           }]
+    //save in local Storage
+    localStorage.setItem("toDo", JSON.stringify(textEl))
 
-    saveArr.push(toDo)
+   
 
-    console.log(toDo)
+});
 
-    var schedule = JSON.stringify(saveArr)
+console.log(textEl)
 
-    if (localPull === null) {
-        JSON.stringify(schedule)
-        localStorage.setItem("itinerary", schedule)
+//Moment = time (.past)
+// if (hourText <  time) {
+//     textEl.addClass("past");
+//     textEl.removeClass("present");
+//     textEl.removeClass("future");
+
+// //Moment = time (.present)
+// } else if (hourText == time) {
+//     textEl.addClass("present");
+//     textEl.removeClass("past")
+//     textEl.removeClass("future")
+
+// //Moment < time (.future)
+// } else {
+//     textEl.addClass("future");
+//     textEl.removeClass("past")
+//     textEl.removeClass("present")
+// }
+
+var time = moment();
+
+function trackTime() {
+    var timeSlot = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+    for (var i = 0; i < timeSlot.length; i++) {
+        var hour = timeSlot [i];
+
+    if (time > hourEl) {
+        document.querySelectorAll('.hour').forEach(function (dscrpB) {
+            dscrpB.classList.add('past');
+            console.log('Before worked');
+          });
+
+    } else if (time == hourEl) {
+        document.querySelectorAll('.hour').forEach(function (dscrpB) {
+            dscrpB.classList.add('present');
+            console.log('Present worked');
+          });
+
     } else {
-        saveArr.push(JSON.parse(localStorage.getItem("itinerary")))
-        localStorage.setItem("itinerary", schedule)
-    }
-};
-saveBtnEl.click(saveText)
-console.log(saveBtnEl)
-
-
-function renderMessage() {
-
-    if (localPull !== null) {
-        
-    }
-  }
-
-
-//Local storage gets item and renders on field
-
-//User inputs text into section 
-
-//save button is clicked in section
-
-if (hourText <  timeText) {
-    textEl.addClass("past");
-    textEl.removeClass("present");
-    textEl.removeClass("future");
-
-} else if (hourText === timeText) {
-    textEl.
-    textEl.addClass("present");
-    textEl.removeClass("past")
-    textEl.removeClass("future")
-
-} else {
-    textEl.addClass("future");
-    textEl.removeClass("past")
-    textEl.removeClass("present")
+        document.querySelectorAll('.hour').forEach(function (dscrpB) {
+            dscrpB.classList.add('future');
+            console.log('After worked');
+          });
 }
-
+    }};
+trackTime();
