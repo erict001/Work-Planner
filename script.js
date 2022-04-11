b0d5f05b8ee67f79336daf253e1ed147f51bc763
Eric Taylor <eric.taylor1012@gmail.com>
2022-04-10 22:39:57 -0700

    working planner

--- script.js
+++ script.js
@@ -1,126 +1,65 @@
-$(document).ready(function(saveText) {
-    //listen for button clicks
-    $('.saveBtn').on('click', function () {
-       //get nearby values/time
-       var toDo = [{
-                work: $(this).siblings("input").val(),
-                hour: $(this).siblings("div").text(),
-            }]
-
-                saveArr.push(toDo)
-                console.log(toDo)
-                var schedule = JSON.stringify(saveArr)
-                
-        //save in local Storage
-        function saveText() {
-            textDiv.textContent = savedText
-            localStorage.setItem("textSaved", savedText)
-        }
-    });
-
-    function getText() {
-        var storedText = localStorage.getItem("textSaved")
-    }
-
 var currentDayEl = $('#currentDay')
 var hourEl = $(".hour")
 var textEl = $(".textDiv")
 var saveBtnEl = $(".btn")
+var coloredArea = $(".hour")
 var localPull = JSON.parse(localStorage.getItem("itinerary"))
 var saveArr = []
-// var finish = localPull[]
-// var hourText = Number(hourEl.text())
-// var timeText = Number(moment().format("HH"))
+
 
 //use moment.js to display current date
 setInterval(function() {
-    var time = moment().format("MMMM Qo, YYYY");
+    var time = moment().format("MMMM Do, YYYY, h:mm:ss a");
 $("#currentDay").text(time); 
 },1000);
 
-// function saveText() {
-//     var toDo = [{
-//         work: $(this).siblings("input").val(),
-//         hour: $(this).siblings("div").text(),
-//     }]
-
-//     saveArr.push(toDo)
-
-//     console.log(toDo)
-
-//     var schedule = JSON.stringify(saveArr)
-
-//     if (localPull === null) {
-//         JSON.stringify(schedule)
-//         localStorage.setItem("itinerary", schedule)
-//     } else {
-//         why
-//         saveArr.push(JSON.parse(localStorage.getItem("itinerary")))
-//         localStorage.setItem("itinerary", schedule)
-//     }
-// };
-// saveBtnEl.click(saveText)
-// console.log(saveBtnEl)
-
-
-// function renderMessage() {
-
-//     if (localPull !== null) {
-        
-//     }
-//   }
-
-//save text to console
-
-
-
-
-//Moment = time (.past)
-// if (hourText <  time) {
-//     textEl.addClass("past");
-//     textEl.removeClass("present");
-//     textEl.removeClass("future");
-
-// //Moment = time (.present)
-// } else if (hourText == time) {
-//     textEl.addClass("present");
-//     textEl.removeClass("past")
-//     textEl.removeClass("future")
-
-// //Moment < time (.future)
-// } else {
-//     textEl.addClass("future");
-//     textEl.removeClass("past")
-//     textEl.removeClass("present")
-// }
-
-var time = moment();
-
+// Function to track time to determine block colors
 function trackTime() {
-    var timeSlot = [0900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700];
+    var now = moment().hours();
 
-    for (var i = 0; i < timeSlot.length; i++) {
-        var hour = timeSlot [i];
-
-    if (time < hourEl) {
-        document.querySelectorAll('.hour').forEach(function (dscrpB) {
-            dscrpB.classList.add('past');
+    coloredArea.each(function() {
+    let colorBlocks = $(this).attr("id");
+  
+    if (colorBlocks < now) {
+        $(this).addClass("past")
             console.log('Before worked');
-          });
 
-    } else if (time == hourEl) {
-        document.querySelectorAll('.hour').forEach(function (dscrpB) {
-            dscrpB.classList.add('present');
+    } else if (colorBlocks === now) {
+        $(this).addClass("present")
             console.log('Present worked');
-          });
 
     } else {
-        document.querySelectorAll('.hour').forEach(function (dscrpB) {
-            dscrpB.classList.add('future');
+        $(this).addClass("future")
             console.log('After worked');
-          });
-}
-    }};
-trackTime();
+          };
+    });
+};
 
-});
+//save upon button click
+saveBtnEl.on("click", function() {
+
+    //save information
+    var time = $(this).siblings(".hour").text();
+    var text = $(this).siblings(".textDiv").val();
+
+    //then text is saved in localStorage   
+    localStorage.setItem(time, text)
+})
+
+//upon refresh of the page - saved item remains
+function planner () {
+
+    $(".hour").each(function() {
+    var currentHour = $(this).text();
+    var plannerText = localStorage.getItem(currentHour);
+
+    if(plannerText !== null) {
+        $(this).siblings(".textDiv").val(plannerText)
+    }
+
+    });
+}
+
+// Callback Function
+trackTime();
+planner();
